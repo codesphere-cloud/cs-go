edcb72cbabaaf83a90fd649e0daa1f771c794f74
schrodit <mail@timschrodi.tech>
2025-04-23 22:55:37 +0200

    feat(deploy): add utils to deploy a workspace

---
+++ pkg/cs/errors.go
@@ -0,0 +1,48 @@
+package cs
+
+import (
+	"fmt"
+	"time"
+)
+
+type TimedOut struct {
+	msg string
+}
+
+func (e *TimedOut) Error() string {
+	return e.msg
+}
+
+func NewTimedOut(operation string, timeout time.Duration) *TimedOut {
+	return &TimedOut{
+		msg: fmt.Sprintf("%s timed out after %s", operation, timeout.String()),
+	}
+}
+
+type NotFound struct {
+	msg string
+}
+
+func (e *NotFound) Error() string {
+	return e.msg
+}
+
+func NewNotFound(msg string) *NotFound {
+	return &NotFound{
+		msg: msg,
+	}
+}
+
+type Duplicated struct {
+	msg string
+}
+
+func (e *Duplicated) Error() string {
+	return e.msg
+}
+
+func NewDuplicated(msg string) *Duplicated {
+	return &Duplicated{
+		msg: msg,
+	}
+}
