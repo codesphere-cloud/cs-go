553f9eb505dc87782067d416498fe76da90ce62e
Manuel Dewald <manuel.dewald@codesphere.com>
2025-05-09 12:22:38 +0200

    devx(release): Add auto-tagging action
    Signed-off-by: Manuel Dewald <manuel.dewald@codesphere.com>

---
+++ hack/tag.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -eu
+BRANCH=$(git branch --show-current)
+if [[ "$BRANCH" != "main" ]]; then
+  echo "This script should only run on main branch, not $BRANCH"
+  exit 1
+fi
+TAG=$(git tag | sort -uV | tail -n 1)
+LOG=$(git log "$TAG..HEAD"  --pretty=format:%s)
+BREAK=$(grep -e '!' >/dev/null <<< "$LOG"; echo $?)
+FEAT=$(grep -e '^feat' >/dev/null <<< "$LOG"; echo $?)
+FIX=$(grep -e '^fix' >/dev/null <<< "$LOG"; echo $?)
+
+echo "Latest tag: $TAG"
+echo "------"
+echo "Relevant changes:"
+echo "$LOG"
+echo "------"
+
+NEWTAG="$TAG"
+
+if [[ $BREAK -eq 0 ]]; then
+  echo "Breaking change! Increasing major."
+  NEWTAG=$(sed -r 's/v([0-9]+)\.[0-9]+\.[0-9]+/echo "v$((\1+1)).0.0"/e' <<< "$TAG")
+elif [[ $FEAT -eq 0 ]]; then
+  echo "New feature! Increasing minor."
+  NEWTAG=$(sed -r 's/v([0-9])+\.([0-9]+)\.[0-9]+/echo "v\1.$((\2+1)).0"/e' <<< "$TAG")
+elif [[ $FIX -eq 0 ]]; then
+  echo "New fix! Increasing patch."
+  NEWTAG=$(sed -r 's/v([0-9])+\.([0-9]+)\.([0-9]+)/echo "v\1.\2.$((\3+1))"/e' <<< "$TAG")
+fi
+
+if [[ $NEWTAG == $TAG ]]; then
+  echo "Nothing to tag."
+  exit 0
+fi
+
+git tag "$NEWTAG"
+git push origin "$NEWTAG"
