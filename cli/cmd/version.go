40c047b72b8b7446c9049356d7e978ac88a7d777
Manuel Dewald <manuel@codesphere.com>
2025-06-03 14:43:32 +0200

    add description to version command
    Signed-off-by: Manuel Dewald <manuel@codesphere.com>

--- cli/cmd/version.go
+++ cli/cmd/version.go
@@ -27,13 +27,8 @@ func AddVersionCmd(rootCmd *cobra.Command) {
 	version := VersionCmd{
 		cmd: &cobra.Command{
 			Use:   "version",
-			Short: "A brief description of your command",
-			Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+			Short: "Print version",
+			Long:  `Prints current version of Codesphere CLI.`,
 		},
 	}
 	rootCmd.AddCommand(version.cmd)
