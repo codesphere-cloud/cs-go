2a8cb3a218bc7e8473309fdc7d8e5e636b0c654d
Manuel Dewald <manuel@codesphere.com>
2025-06-04 06:47:38 +0200

    devx(gh-action): Fix docs generation action
    Signed-off-by: Manuel Dewald <manuel@codesphere.com>

--- hack/update-docs.sh
+++ hack/update-docs.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+# Copyright (c) Codesphere Inc.
+# SPDX-License-Identifier: Apache-2.0
 
-make generate-licenses
+
+set -euo pipefail
+
+make generate-license
 make docs
-if [[ `git status --porcelain` ]]; then
+if [[ $(git status --porcelain) ]]; then
   git add .
+  git config --global user.name "${GITHUB_ACTOR}"
+  git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
   git commit -m "docs(licenses,usage): Update docs & licenses"
   git push origin main
 fi
