ee81c2f67fd168c573983c9d9ffc426718a1cb07
Datata1 <jdwiederstein01@gmail.com>
2025-06-11 10:54:09 +0200

    resolve PR comments
    Signed-off-by: Datata1 <jdwiederstein01@gmail.com>

--- cli/cmd/set-env-vars.go
+++ cli/cmd/set-env-vars.go
@@ -31,7 +31,6 @@ func AddSetEnvVarCmd(p *cobra.Command, opts GlobalOptions) {
 			Use:   "set-env",
 			Short: "Set environment varariables",
 			Long:  `Set environment variables in a workspace from flags or a .env file.`,
-			// BEISPIELE AKTUALISIERT
 			Example: io.FormatExampleCommands("set-env", []io.Example{
 				{Cmd: "--workspace <id> --env-var FOO=bar", Desc: "Set a single environment variable"},
 				{Cmd: "--workspace <id> --env-var FOO=bar --env-var HELLO=world", Desc: "Set multiple environment variables"},
@@ -59,34 +58,55 @@ func (l *SetEnvVarCmd) RunE(_ *cobra.Command, args []string) (err error) {
 
 	return l.SetEnvironmentVariables(client)
 }
+func ReadEnvFile(l *SetEnvVarCmd, finalEnvVarMap map[string]string) error {
+
+	envFile := *l.Opts.EnvFile
+	if _, err := os.Stat(envFile); os.IsNotExist(err) {
+		return fmt.Errorf("env file does not exist: %s", envFile)
+	}
+
+	fileEnvMap, err := godotenv.Read(envFile)
+	if err != nil {
+		return fmt.Errorf("failed to parse env file %s: %w", envFile, err)
+	}
+
+	for key, value := range fileEnvMap {
+		finalEnvVarMap[key] = value
+	}
+
+	return nil
+}
+
+func ReadEnvVarArg(l *SetEnvVarCmd, finalEnvVarMap map[string]string) error {
+
+	flagEnvVarMap, err := cs.ArgToEnvVarMap(*l.Opts.EnvVar)
+	if err != nil {
+		return fmt.Errorf("failed to parse environment variables from flags: %w", err)
+	}
+
+	for key, value := range flagEnvVarMap {
+		finalEnvVarMap[key] = value
+	}
+
+	return nil
+}
 
 func (l *SetEnvVarCmd) SetEnvironmentVariables(client Client) (err error) {
 	finalEnvVarMap := make(map[string]string)
 
+	// Check if the environment file is provided and read it
 	if l.Opts.EnvFile != nil && *l.Opts.EnvFile != "" {
-		envFile := *l.Opts.EnvFile
-		if _, err := os.Stat(envFile); os.IsNotExist(err) {
-			return fmt.Errorf("env file does not exist: %s", envFile)
-		}
-
-		fileEnvMap, err := godotenv.Read(envFile)
+		err := ReadEnvFile(l, finalEnvVarMap)
 		if err != nil {
-			return fmt.Errorf("failed to parse env file %s: %w", envFile, err)
-		}
-
-		for key, value := range fileEnvMap {
-			finalEnvVarMap[key] = value
+			return fmt.Errorf("failed to read environment variables from file: %w", err)
 		}
 	}
 
+	// Check if the environment variables are provided as flags and read them
 	if l.Opts.EnvVar != nil && len(*l.Opts.EnvVar) > 0 {
-		flagEnvVarMap, err := cs.ArgToEnvVarMap(*l.Opts.EnvVar)
+		err := ReadEnvVarArg(l, finalEnvVarMap)
 		if err != nil {
-			return fmt.Errorf("failed to parse environment variables from flags: %w", err)
-		}
-
-		for key, value := range flagEnvVarMap {
-			finalEnvVarMap[key] = value
+			return fmt.Errorf("failed to read environment variables from flags: %w", err)
 		}
 	}
 
