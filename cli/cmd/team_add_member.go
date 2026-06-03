368d674e3fc557cc91e9fbed7329d4f1764078b1
Maximilian <burr.maximilian97@gmail.com>
2026-06-03 10:19:04 +0200

    add team management features
    Signed-off-by: Maximilian <burr.maximilian97@gmail.com>

--- cli/cmd/team_add_member.go
+++ cli/cmd/team_add_member.go
@@ -4,12 +4,23 @@
 package cmd
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
 type AddTeamMemberCmd struct {
-	cmd  *cobra.Command
-	Opts CreateWorkspaceOpts
+	cmd           *cobra.Command
+	Opts          AddTeamMemberOpts
+	ClientFactory func(GlobalOptions) (Client, error)
+}
+
+type AddTeamMemberOpts struct {
+	*GlobalOptions
+	Email  string
+	Role   int
+	TeamId int
 }
 
 func AddAddTeamMemberCmd(team *cobra.Command, opts *GlobalOptions) {
@@ -19,14 +30,44 @@ func AddAddTeamMemberCmd(team *cobra.Command, opts *GlobalOptions) {
 			Short: "Add team member",
 			Long:  `Add team member to a team`,
 		},
-		Opts: CreateWorkspaceOpts{
+		Opts: AddTeamMemberOpts{
 			GlobalOptions: opts,
 		},
+		ClientFactory: NewClient,
 	}
 	t.cmd.RunE = t.RunE
+	t.cmd.Flags().StringVarP(&t.Opts.Email, "email", "e", "", "Team member email")
+	t.cmd.Flags().IntVarP(&t.Opts.Role, "role", "r", 0, "Team member role 0=admin, 1=member")
 	AddCmd(team, t.cmd)
 }
 
 func (c *AddTeamMemberCmd) RunE(_ *cobra.Command, args []string) error {
+	client, err := c.ClientFactory(*c.Opts.GlobalOptions)
+	if err != nil {
+		return fmt.Errorf("failed to create Codespehre client: %w", err)
+	}
+
+	teamId, err := c.Opts.GetTeamId()
+	if err != nil {
+		return err
+	}
+
+	err = c.AddTeamMember(client, teamId, c.Opts.Email, c.Opts.Role)
+	return err
+
+}
+
+func (c *AddTeamMemberCmd) AddTeamMember(client Client, teamId int, email string, role int) error {
+	if email == "" {
+		return errors.New("email cannot be empty")
+	}
+
+	fmt.Printf("add member: %s to team %d with role: %d", email, teamId, role)
+
+	err := client.AddTeamMember(teamId, email, role)
+	if err != nil {
+		return fmt.Errorf("failed to add member to team: %w", err)
+	}
+
 	return nil
 }
