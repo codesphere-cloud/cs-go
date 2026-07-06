0c58e0f7a1aac65503632a80427e969a68ce77a1
Maximilian <burr.maximilian97@gmail.com>
2026-07-06 16:27:51 +0200

    refac: removed unused TeamIdByName function
    Signed-off-by: Maximilian <burr.maximilian97@gmail.com>

--- api/team.go
+++ api/team.go
@@ -4,40 +4,9 @@
 package api
 
 import (
-	"fmt"
-
 	cserrors "github.com/codesphere-cloud/cs-go/api/errors"
 	"github.com/codesphere-cloud/cs-go/api/openapi_client"
 )
-
-// Fetches the team for a given team name.
-// If the name is ambigous an error is thrown.
-//
-// Returns [NotFound] if no plan with the given Id could be found
-// Returns [Duplicated] if no plan with the given Id could be found
-func (client *Client) TeamIdByName(name string) (Team, error) {
-	teams, err := client.ListTeams("")
-	if err != nil {
-		return Team{}, err
-	}
-
-	matchingTeams := []Team{}
-	for _, t := range teams {
-		if t.Name == name {
-			matchingTeams = append(matchingTeams, t)
-		}
-	}
-
-	if len(matchingTeams) == 0 {
-		return Team{}, cserrors.NotFound(fmt.Sprintf("no team with name %s found", name))
-	}
-
-	if len(matchingTeams) > 1 {
-		return Team{}, cserrors.Duplicated(fmt.Sprintf("multiple teams (%v) with the name %s found.", matchingTeams, name))
-	}
-
-	return matchingTeams[0], nil
-}
 
 func (c *Client) ListTeams(orgId string) ([]Team, error) {
 	if orgId != "" {
