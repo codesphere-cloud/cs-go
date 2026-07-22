58bfad4c6261dd82edfb942d030e688a4bc9cf0b
Maximilian <burr.maximilian97@gmail.com>
2026-07-22 18:05:09 +0200

    added more test cases
    Signed-off-by: Maximilian <burr.maximilian97@gmail.com>

--- api/organizations_test.go
+++ api/organizations_test.go
@@ -5,6 +5,9 @@ package api_test
 
 import (
 	"context"
+	"errors"
+	"net/http"
+	"reflect"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -21,14 +24,14 @@ var _ = Describe("Organizations", func() {
 		client               *api.Client
 	)
 
-	BeforeEach(func() {
+	BeforeEach(func(ctx SpecContext) {
 		clustersApiMock = openapi_client.NewMockClustersAPI(GinkgoT())
 		organizationsApiMock = openapi_client.NewMockOrganizationsAPI(GinkgoT())
 		apis := openapi_client.APIClient{
 			ClustersAPI:      clustersApiMock,
 			OrganizationsAPI: organizationsApiMock,
 		}
-		client = api.NewClientWithCustomDeps(context.TODO(), api.Configuration{}, &apis, mockTime())
+		client = api.NewClientWithCustomDeps(ctx, api.Configuration{}, &apis, mockTime())
 	})
 
 	Context("ListOrganizations", func() {
@@ -46,6 +49,34 @@ var _ = Describe("Organizations", func() {
 			Expect(err).NotTo(HaveOccurred())
 			Expect(orgs).To(Equal(expectedOrgs))
 		})
+
+		It("handles HTTP 500 errors when the API fails", func() {
+			apiErr := errors.New("internal server error")
+			organizationsApiMock.EXPECT().OrganizationsListOrganizations(mock.Anything).
+				Return(openapi_client.ApiOrganizationsListOrganizationsRequest{ApiService: organizationsApiMock})
+			organizationsApiMock.EXPECT().OrganizationsListOrganizationsExecute(mock.Anything).
+				Return(nil, &http.Response{StatusCode: 500}, apiErr)
+
+			orgs, err := client.ListOrganizations()
+
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("internal server error"))
+			Expect(orgs).To(BeNil())
+		})
+
+		It("handles HTTP 403 forbidden errors", func() {
+			apiErr := errors.New("forbidden")
+			organizationsApiMock.EXPECT().OrganizationsListOrganizations(mock.Anything).
+				Return(openapi_client.ApiOrganizationsListOrganizationsRequest{ApiService: organizationsApiMock})
+			organizationsApiMock.EXPECT().OrganizationsListOrganizationsExecute(mock.Anything).
+				Return(nil, &http.Response{StatusCode: 403}, apiErr)
+
+			orgs, err := client.ListOrganizations()
+
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("forbidden"))
+			Expect(orgs).To(BeNil())
+		})
 	})
 
 	Context("CreateOrganization", func() {
@@ -59,13 +90,64 @@ var _ = Describe("Organizations", func() {
 
 			clustersApiMock.EXPECT().ClustersCreateOrganization(mock.Anything).
 				Return(openapi_client.ApiClustersCreateOrganizationRequest{ApiService: clustersApiMock})
-			clustersApiMock.EXPECT().ClustersCreateOrganizationExecute(mock.Anything).Return(&expectedOrg, nil, nil)
+			clustersApiMock.EXPECT().ClustersCreateOrganizationExecute(mock.MatchedBy(func(r openapi_client.ApiClustersCreateOrganizationRequest) bool {
+				val := reflect.ValueOf(r)
+				payloadField := val.FieldByName("clustersCreateOrganizationRequest")
+				if !payloadField.IsValid() || payloadField.IsNil() {
+					return false
+				}
+				elem := payloadField.Elem()
+				return elem.FieldByName("Name").String() == orgName &&
+					elem.FieldByName("AdminEmail").String() == adminEmail
+			})).Return(&expectedOrg, nil, nil)
 
 			org, err := client.CreateOrganization(orgName, adminEmail)
 
 			Expect(err).NotTo(HaveOccurred())
 			Expect(org.Id).To(Equal(expectedOrg.Id))
 			Expect(org.Name).To(Equal(expectedOrg.Name))
+		})
+
+		It("handles empty strings and invalid email formats from API errors", func() {
+			orgName := ""
+			adminEmail := "invalid-email"
+			apiErr := errors.New("bad request: invalid payload")
+
+			clustersApiMock.EXPECT().ClustersCreateOrganization(mock.Anything).
+				Return(openapi_client.ApiClustersCreateOrganizationRequest{ApiService: clustersApiMock})
+			clustersApiMock.EXPECT().ClustersCreateOrganizationExecute(mock.MatchedBy(func(r openapi_client.ApiClustersCreateOrganizationRequest) bool {
+				val := reflect.ValueOf(r)
+				payloadField := val.FieldByName("clustersCreateOrganizationRequest")
+				if !payloadField.IsValid() || payloadField.IsNil() {
+					return false
+				}
+				elem := payloadField.Elem()
+				return elem.FieldByName("Name").String() == orgName &&
+					elem.FieldByName("AdminEmail").String() == adminEmail
+			})).Return(nil, &http.Response{StatusCode: 400}, apiErr)
+
+			org, err := client.CreateOrganization(orgName, adminEmail)
+
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(ContainSubstring("invalid payload"))
+			Expect(org).To(BeNil())
+		})
+
+		It("handles context timeouts", func() {
+			orgName := "timeoutOrg"
+			adminEmail := "timeout@example.com"
+			apiErr := context.DeadlineExceeded
+
+			clustersApiMock.EXPECT().ClustersCreateOrganization(mock.Anything).
+				Return(openapi_client.ApiClustersCreateOrganizationRequest{ApiService: clustersApiMock})
+			clustersApiMock.EXPECT().ClustersCreateOrganizationExecute(mock.Anything).
+				Return(nil, nil, apiErr)
+
+			org, err := client.CreateOrganization(orgName, adminEmail)
+
+			Expect(err).To(HaveOccurred())
+			Expect(errors.Is(err, context.DeadlineExceeded)).To(BeTrue())
+			Expect(org).To(BeNil())
 		})
 	})
 })
